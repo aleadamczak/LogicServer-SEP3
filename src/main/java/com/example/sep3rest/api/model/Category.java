15984f5e02af21150bf3c397c3a7726f89bb41f9
Domonkos Papp <325338@via.dk>
2023-05-09 22:48:08 +0200

    Bruh

---
+++ src/main/java/com/example/sep3rest/api/model/Category.java
@@ -0,0 +1,27 @@
+package com.example.sep3rest.api.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Category {
+
+    @JsonProperty
+    private String name;
+
+
+    @JsonProperty
+    private int id;
+
+    @JsonCreator
+    public Category(@JsonProperty("name") String name,
+                    @JsonProperty("id") int id){
+        this.name = name;
+        this.id= id;
+    }
+
+
+
+    public String getName(){
+        return name;
+    }
+}
